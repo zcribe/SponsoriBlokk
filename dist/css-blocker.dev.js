e8ffdd6f4f26d74826de87197bf89bf8a7bb74e4
zcribe <erlend.eelmets@gmail.com>
2020-10-05 09:51:48 +0300

    Refactor code for more DRY

---
+++ dist/css-blocker.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var tags = [".adform-video-base ", ".adform-video-state-finished", "video-container", "#adf-scroller", "#cookie_ribbon", "iframe", "#delfi-adx-siteheader", ".adform-adbox", ".adfm-cube-vertical", ".customblock", // Postimees
+".GoogleActiveViewInnerContainer", "#ad-container", ".scroller", "#Stage", ".escobar-banner", ".GoogleActiveViewElement", ".animation_container", ".list-article--commercial", ".img_ad", ".commercial-articles", ".diamond__image", ".adf-scroller", ".dfp-ad", ".digipakett-branding__container", ".notification--cookie", ".notifications-container", "#background-11753060", ".group-topic-with-custom-header--basic-logo-header", "article[commercial-tag]", ".diamond", ".group-topic-with-custom-header--topic"];
+var sponsored = document.querySelectorAll(tags.toString());
+sponsored.forEach(function (articleItem) {
+  articleItem.remove();
+});
--- postimees.js
+++
@@ -1,5 +0,0 @@
-var sponsored = document.querySelectorAll(".GoogleActiveViewInnerContainer, #ad-container, .scroller, #Stage, .escobar-banner, .GoogleActiveViewElement, .animation_container, .list-article--commercial, .img_ad, .commercial-articles, .diamond__image , .adf-scroller, .dfp-ad, .digipakett-branding__container, .notification--cookie, .notifications-container, #background-11753060, .group-topic-with-custom-header--basic-logo-header, article[commercial-tag],.diamond, .group-topic-with-custom-header--topic")
-
-sponsored.forEach(function(articleItem){
-    articleItem.remove()
-})
